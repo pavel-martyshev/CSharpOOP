3d94912609e9602f278b198b5a8545fc913f8af0
Pavel <pasha.martyshev@gmail.com>
2025-01-29 10:33:34 +0300

    Created RangeTask

---
+++ RangeTask/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace RangeTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rangeStart = 1;
+            double rangeEnd = 5;
+
+            Range range = new(rangeStart, rangeEnd);
+
+            //Console.Write("Введите начало диапазона:" + Environment.NewLine + "> ");
+            //double rangeStart = double.Parse(Console.ReadLine()!.Replace('.', ','));
+
+            //Console.Write("Введите конец диапазона:" + Environment.NewLine + "> ");
+            //double rangeEnd = double.Parse(Console.ReadLine()!.Replace('.', ','));
+
+            //Range range = new(rangeStart, rangeEnd);
+
+            //Console.WriteLine($"Длина диапазона: {range.GetLength():f2}");
+
+            //Console.Write("Введите число для проверки:" + Environment.NewLine + "> ");
+            //double numberToCheck = double.Parse(Console.ReadLine()!.Replace('.', ','));
+
+            //if (range.IsInside(numberToCheck))
+            //{
+            //    Console.WriteLine($"Число {numberToCheck} находится внутри диапазона от {range.From} до {range.To}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Число {numberToCheck} находится вне диапазона от {range.From} до {range.To}");
+            //}
+        }
+    }
+}
---
+++ RangeTask/Range.cs
@@ -0,0 +1,18 @@
+﻿namespace RangeTask
+{
+    internal class Range(double from, double to)
+    {
+        public double From { get; set; } = from;
+        public double To { get; set; } = to;
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+    }
+}
