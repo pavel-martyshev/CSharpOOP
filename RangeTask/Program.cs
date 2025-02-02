686b422b5f452175f9a5823bbe7a364ca3ea9c4e
Pavel <pasha.martyshev@gmail.com>
2025-02-02 19:03:40 +0300

    Class Range with the required functionality and a program for checking its functionality have been implemented

--- RangeTask/Program.cs
+++ RangeTask/Program.cs
@@ -4,32 +4,68 @@
     {
         static void Main(string[] args)
         {
-            double rangeStart = 1;
-            double rangeEnd = 5;
+            Console.Write("Введите начало интервала:" + Environment.NewLine + "> ");
+            double rangeStart = double.Parse(Console.ReadLine()!.Replace('.', ','));
+
+            Console.Write("Введите конец интервала:" + Environment.NewLine + "> ");
+            double rangeEnd = double.Parse(Console.ReadLine()!.Replace('.', ','));
 
             Range range = new(rangeStart, rangeEnd);
 
-            //Console.Write("Введите начало диапазона:" + Environment.NewLine + "> ");
-            //double rangeStart = double.Parse(Console.ReadLine()!.Replace('.', ','));
+            Console.WriteLine($"Длина интервала: {range.GetLength():f2}");
 
-            //Console.Write("Введите конец диапазона:" + Environment.NewLine + "> ");
-            //double rangeEnd = double.Parse(Console.ReadLine()!.Replace('.', ','));
+            Console.Write("Введите число для проверки:" + Environment.NewLine + "> ");
+            double numberToCheck = double.Parse(Console.ReadLine()!.Replace('.', ','));
 
-            //Range range = new(rangeStart, rangeEnd);
+            if (range.IsInside(numberToCheck))
+            {
+                Console.WriteLine($"Число {numberToCheck} находится внутри интервала от {range.From} до {range.To}");
+            }
+            else
+            {
+                Console.WriteLine($"Число {numberToCheck} находится вне интервала от {range.From} до {range.To}");
+            }
 
-            //Console.WriteLine($"Длина диапазона: {range.GetLength():f2}");
+            Console.Write("Введите начало второго интервала:" + Environment.NewLine + "> ");
+            double range2Start = double.Parse(Console.ReadLine()!.Replace('.', ','));
 
-            //Console.Write("Введите число для проверки:" + Environment.NewLine + "> ");
-            //double numberToCheck = double.Parse(Console.ReadLine()!.Replace('.', ','));
+            Console.Write("Введите конец интервала:" + Environment.NewLine + "> ");
+            double range2End = double.Parse(Console.ReadLine()!.Replace('.', ','));
 
-            //if (range.IsInside(numberToCheck))
-            //{
-            //    Console.WriteLine($"Число {numberToCheck} находится внутри диапазона от {range.From} до {range.To}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"Число {numberToCheck} находится вне диапазона от {range.From} до {range.To}");
-            //}
+            Range range2 = new(range2Start, range2End);
+            Range? intersection = range.GetIntersection(range2);
+
+            if (intersection is not null)
+            {
+                Console.WriteLine($"Интервалы {range} и {range2} пересекаются в точках {intersection}");
+            }
+            else
+            {
+                Console.WriteLine($"Интервалы {range} и {range2} не пересекаются");
+            }
+
+            Console.WriteLine($"Результат объединения интервалов {range} и {range2}:");
+
+            foreach (Range interval in range.UnionIntervals(range2))
+            {
+                Console.WriteLine(interval);
+            }
+
+            Range[]? intervalsDifference = range.GetIntervalsDifference(range2);
+
+            if (intervalsDifference is not null)
+            {
+                Console.WriteLine($"Разность интервалов {range} и {range2}:");
+
+                foreach (Range difference in intervalsDifference)
+                {
+                    Console.WriteLine(difference);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Не удалось найти разность интервалов {range} и {range2}");
+            }
         }
     }
 }
--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -5,6 +5,12 @@
         public double From { get; set; } = from;
         public double To { get; set; } = to;
 
+
+        public override string ToString()
+        {
+            return $"({From}, {To})";
+        }
+
         public double GetLength()
         {
             return To - From;
@@ -13,6 +19,54 @@
         public bool IsInside(double number)
         {
             return number >= From && number <= To;
+        }
+
+        public Range? GetIntersection(Range range)
+        {
+            if (From <= range.To && range.From <= To)
+            {
+                double startIntersection = Math.Max(From, range.From);
+                double endIntersection = Math.Min(To, range.To);
+
+                if (startIntersection != endIntersection)
+                {
+                    return new Range(startIntersection, endIntersection);
+                }
+
+                return null;
+            }
+
+            return null;
+        }
+
+        public Range[] UnionIntervals(Range range)
+        {
+            if (From != range.From)
+            {
+                return [new Range(From, Math.Max(To, range.To)), new Range(range.From, Math.Max(To, range.To))];
+            }
+
+            return [new Range(From, Math.Max(To, range.To))];
+        }
+
+        public Range[]? GetIntervalsDifference(Range range)
+        {
+            if (From < range.From && To > range.To)
+            {
+                return [new Range(From, range.From), new Range(range.To, To)];
+            }
+
+            if (From < range.From && To == range.To)
+            {
+                return [new Range(From, range.From)];
+            }
+
+            if (From == range.From && To > range.To)
+            {
+                return [new Range(range.To, To)];
+            }
+
+            return null;
         }
     }
 }
