f3ed27ca999753589318528adde07d53bd178b9e
Pavel <pasha.martyshev@gmail.com>
2025-02-13 23:04:16 +0300

    Complited VectorTask

---
+++ VectorTask/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace VectorTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new(3);
+            vector1[0] = 1;
+            vector1[1] = 2;
+            vector1[2] = 3;
+
+            Console.WriteLine($"Вектор 1: {vector1}");
+            Console.WriteLine($"Длина вектора 1: {vector1.GetLength()}");
+
+            Console.WriteLine();
+
+            Vector vector2 = new([4, 5, 6]);
+
+            Console.WriteLine($"Вектор 2: {vector2}");
+            Console.WriteLine($"Длина вектора 2: {vector2.GetLength()}");
+
+            Console.WriteLine();
+
+            Vector vector3 = Vector.AdditionTwoVectors(vector1, vector2);
+            Console.WriteLine($"Результат сложения вектора 1 и вектора 2: {vector3}");
+
+            Console.WriteLine();
+
+            Vector vector4 = Vector.SubtractionTwoVectors(vector2, vector1);
+            Console.WriteLine($"Результат вычитания вектора 1 из вектора 2: {vector4}");
+
+            Console.WriteLine();
+
+            double dotProduct = Vector.GetDotProduct(vector1, vector2);
+            Console.WriteLine($"Скалярное произведение вектора 1 и вектора 2: {dotProduct}");
+        }
+    }
+}
---
+++ VectorTask/Vector.cs
@@ -0,0 +1,184 @@
+﻿namespace VectorTask;
+
+internal class Vector
+{
+    public List<double> Components { get; set; }
+
+    public static Vector AdditionTwoVectors(Vector vector1, Vector vector2)
+    {
+        Vector resultVector = new(vector1);
+        resultVector.Add(vector2);
+
+        return resultVector;
+    }
+
+    public static Vector SubtractionTwoVectors(Vector vector1, Vector vector2)
+    {
+        Vector resultVector = new(vector1);
+        resultVector.Subtract(vector2);
+
+        return resultVector;
+    }
+
+    public static double GetDotProduct(Vector vector1, Vector vector2)
+    {
+        int smallestComponentsSize = Math.Min(vector1.GetSize(), vector2.GetSize());
+        double dotProduct = 0;
+
+        for (int i = 0; i < smallestComponentsSize; i++)
+        {
+            dotProduct += vector1[i] * vector2[i];
+        }
+
+        return dotProduct;
+    }
+
+    public Vector(int size)
+    {
+        if (size <= 0)
+        {
+            throw new ArgumentException("Size must be > 0", nameof(size));
+        }
+
+        Components = Enumerable.Repeat(0.0, size).ToList();
+    }
+
+    public Vector(Vector vector)
+    {
+        Components = [.. vector.Components];
+    }
+
+    public Vector(double[] values)
+    {
+        Components = [.. values];
+    }
+
+    public Vector(int size, double[] values)
+    {
+        if (size <= 0)
+        {
+            throw new ArgumentException("Size must be > 0", nameof(size));
+        }
+
+        Components = [.. values];
+
+        if (size > values.Length)
+        {
+            Components.AddRange(Enumerable.Repeat(0.0, size - values.Length));
+        }
+    }
+
+    public double this[int index]
+    {
+        get
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("index must be > 0", nameof(index));
+            }
+
+            return Components[index];
+        }
+        set
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("index must be > 0", nameof(index));
+            }
+
+            Components[index] = value;
+        }
+    }
+
+    public void Add(Vector vector)
+    {
+        int smallestComponentsSize = Math.Min(Components.Count, vector.GetSize());
+        List<double> componentsCopy = [.. Components.Count > vector.GetSize() ? Components : vector.Components];
+
+        for (int i = 0; i < smallestComponentsSize; i++)
+        {
+            componentsCopy[i] = Components[i] + vector.Components[i];
+        }
+
+        Components = componentsCopy;
+    }
+
+    public void Subtract(Vector vector)
+    {
+        List<double> subtrahendComponentsCopy = [.. vector.Components];
+
+        if (Components.Count < subtrahendComponentsCopy.Count)
+        {
+            Components.AddRange(Enumerable.Repeat(0.0, subtrahendComponentsCopy.Count - Components.Count));
+        }
+        else
+        {
+            subtrahendComponentsCopy.AddRange(Enumerable.Repeat(0.0, Components.Count - subtrahendComponentsCopy.Count));
+        }
+
+        for (int i = 0; i < Components.Count; i++)
+        {
+            Components[i] = Components[i] - subtrahendComponentsCopy[i];
+        }
+    }
+
+    public void MultiplyByScalar(double scalar)
+    {
+        for (int i = 0; i < Components.Count; i++)
+        {
+            Components[i] = Components[i] * scalar;
+        }
+    }
+
+    public void Reverse() => MultiplyByScalar(-1);
+
+    public int GetSize() => Components.Count;
+
+    public double GetLength() => Math.Round(Math.Sqrt(Components.Select(component => Math.Pow(component, 2)).ToList().Sum()), 2, MidpointRounding.AwayFromZero);
+
+    public override string ToString() => $"{{{string.Join(", ", Components)}}}";
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(obj, this))
+        {
+            return true;
+        }
+
+        if (obj is null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Vector vector = (Vector)obj;
+
+        if (Components.Count != vector.GetSize())
+        {
+            return false;
+        }
+
+        for (int i = 0; i < Components.Count; i++)
+        {
+            if (Components[i] != vector.Components[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        const int prime = 31;
+        int hash = 1;
+
+        foreach (double component in Components)
+        {
+            hash += prime * hash + component.GetHashCode();
+        }
+
+        hash = prime * hash + Components.Count;
+        return hash;
+    }
+}
