d32305a3576f4690594827fd940aa2bfc6ceb8b9
Pavel <pasha.martyshev@gmail.com>
2025-03-02 21:31:35 +0300

    Corrected

--- ArrayListHome/Program.cs
+++ ArrayListHome/Program.cs
@@ -1,41 +1,57 @@
-﻿namespace ArrayListHome;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ArrayListHome;
 
 internal class Program
 {
-    static void Main(string[] args)
+    public static void ReadingFile()
     {
         string filePath = Path.Combine("..", "..", "..", "someFile.txt");
 
-        List<string> fileStrings = [];
+        List<string> fileLines = [];
 
-        using (StreamReader reader = new(filePath))
+        try
         {
-            while (!reader.EndOfStream)
+            using (StreamReader reader = new(filePath))
             {
-                fileStrings.Add(reader.ReadLine()!);
+                string line = reader.ReadLine()!;
+
+                while (line is not null)
+                {
+                    fileLines.Add(reader.ReadLine()!);
+                    line = reader.ReadLine()!;
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, fileLines));
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"Файл не найден ({filePath})");
+        }
+    }
+    
+    public static void RemoveEvenNumbers()
+    {
+        List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        for (int i = numbers.Count - 1; i >= 0; i--)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                numbers.RemoveAt(i);
             }
         }
 
-        Console.WriteLine(string.Join(Environment.NewLine, fileStrings));
-        Console.WriteLine();
+        Console.WriteLine(string.Join(Environment.NewLine, numbers));
+    }
 
-        List<int> numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    public static void ExtractUniqueElements()
+    {
+        List<int> numbers = [1, 5, 2, 1, 3, 5];
+        List<int> uniqueNumbers = new(numbers.Count);
 
-        for (int i = 0; i < numbers1.Count; i++)
-        {
-            if (numbers1[i] % 2 == 0)
-            {
-                numbers1.RemoveAt(i);
-            }
-        }
-
-        Console.WriteLine(string.Join(Environment.NewLine, numbers1));
-        Console.WriteLine();
-
-        List<int> numbers2 = [1, 5, 2, 1, 3, 5];
-        List<int> uniqueNumbers = [];
-
-        foreach (int number in numbers2)
+        foreach (int number in numbers)
         {
             if (!uniqueNumbers.Contains(number))
             {
@@ -44,5 +60,18 @@ internal class Program
         }
 
         Console.WriteLine(string.Join(Environment.NewLine, uniqueNumbers));
+    }
+
+    static void Main(string[] args)
+    {
+        ReadingFile();
+
+        Console.WriteLine();
+
+        RemoveEvenNumbers();
+
+        Console.WriteLine();
+
+        ExtractUniqueElements();
     }
 }
